e78c7716e6d64244588562b0cfc04f175ca4c181
Vishal Dubey <vdubey01@gmail.com>
2020-10-01 04:22:15 -0400

    updated website

--- script.js
+++ script.js
@@ -44,45 +44,22 @@ document.addEventListener("DOMContentLoaded", () => {
       })
     });
 
-
-
-
-    var sections = document.querySelectorAll(".page");
+    const links = document.querySelectorAll('.links');
+    const sections = document.querySelectorAll('.page');
     console.log(sections);
-    // The user scrolls
-    window.scroll(function(){
+    function changeLinkState() {
+      let index = sections.length;
+
+      while(--index && window.scrollY + 50 < sections[index].offsetTop) {}
       
-      // currentScroll is the number of pixels the window has been scrolled
-      var currentScroll = window.scrollTop();
-      console.log(currentScroll);
+      links.forEach((link) => link.classList.remove('active'));
+      links[index].classList.add('active');
       console.log('hi');
-      // $currentSection is somewhere to place the section we must be looking at
-      var currentSection;
-      
-      // We check the position of each of the divs compared to the windows scroll positon
-      sections.each(function(){
-        // divPosition is the position down the page in px of the current section we are testing      
-        var divPosition = window.offset().top;
-        
-        // If the divPosition is less the the currentScroll position the div we are testing has moved above the window edge.
-        // the -1 is so that it includes the div 1px before the div leave the top of the window.
-        if( divPosition - 1 < currentScroll ){
-          // We have either read the section or are currently reading the section so we'll call it our current section
-          currentSection = window;
-          
-          // If the next div has also been read or we are currently reading it we will overwrite this value again. This will leave us with the LAST div that passed.
-        }
-        
-        // This is the bit of code that uses the currentSection as its source of ID
-        var id = currentSection.attr('id').split("_")[1];
-        
-        $('a').removeClass('active');
-        $("[href=#"+id+"]").addClass('active');
-        
-      })
+    }
 
-  });
-
+    changeLinkState();
+    window.addEventListener('scroll', changeLinkState);
+    
 });
 
 
